c622d9dbdf2f2f3870c60ca0ad4c9f6366b57d24
cepy <ccruza04@eeducarex.es>
2026-03-05 20:34:26 +0100

    Dase de Batos

---
+++ aprende_db05_obj/Libro.py
@@ -0,0 +1,9 @@
+class Libro:
+    def __init__(self):
+        self.id = None
+        self.isbn = None
+        self.titulo = None
+        self.autor = None
+        self.editorial = None
+        self.fecha_publicacion = None
+        self.descripcion = None
